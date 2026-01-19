8a6b0b6dfb23f28ddca212c11a2d99d7413adebc
Kayda <Trevlaw15@gmail.com>
2026-01-19 15:09:57 -0800

    Finalized hourly motivation script with UTF-8 support

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ MY_PASSWORD = os.environ.get("MY_PASSWORD")
 
 now = dt.datetime.now()
 
-weekday = now.weekday()
-
-if weekday == 0:
-    with open('quotes.txt', 'r') as quote_file:
-        all_quotes = quote_file.readlines()
-        random_quote = random.choice(all_quotes)
+with open('quotes.txt', 'r') as quote_file:
+    all_quotes = quote_file.readlines()
+    random_quote = random.choice(all_quotes)
     # print(random_quote)
 
-    with smtplib.SMTP('smtp.gmail.com', 587) as connection:
-        connection.starttls()
-        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
-        connection.sendmail(from_addr=MY_EMAIL,
-                            to_addrs=MY_EMAIL,
-                            msg=f"Subject:Motivational!\n\n{random_quote}")
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection.starttls()
+    connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+
+    message = f"Subject:Motivational Quote!\n\n{random_quote}"
+
+    connection.sendmail(from_addr=MY_EMAIL,
+                        to_addrs=MY_EMAIL,
+                        msg=message.encode('utf-8'))
 
